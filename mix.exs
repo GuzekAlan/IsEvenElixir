e45bef38f75c29498f26dd923e98db8cb03237e7
Alan Guzek <alan.guzek@swmansion.com>
2025-04-28 13:50:48 +0200

    fix project setup

--- mix.exs
+++ mix.exs
@@ -4,6 +4,11 @@ defmodule IsEven.MixProject do
   def project do
     [
       app: :is_even,
+      description: "A simple Elixir project to check if a number is even.",
+      package: [
+        licenses: ["Apache-2.0"],
+        links: %{github: "https://github.com/GuzekAlan/IsEvenElixir"}
+      ],
       version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
