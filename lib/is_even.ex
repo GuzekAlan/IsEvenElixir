c08c3ee9493e685ccdce08c2cd543b8e14573a6f
Alan Guzek <alan.guzek@swmansion.com>
2025-04-28 13:27:28 +0200

    add body

--- lib/is_even.ex
+++ lib/is_even.ex
@@ -4,15 +4,13 @@ defmodule IsEven do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> IsEven.hello()
-      :world
-
+  Returns number if it is even, otherwise raises an error.
   """
-  def hello do
-    :world
+  def even?(number) when is_integer(number) do
+    if rem(number, 2) == 0 do
+      number
+    else
+      raise ArgumentError, message: "The number #{number} is not even."
+    end
   end
 end
--- test/is_even_test.exs
+++ test/is_even_test.exs
@@ -2,7 +2,13 @@ defmodule IsEvenTest do
   use ExUnit.Case
   doctest IsEven
 
-  test "greets the world" do
-    assert IsEven.hello() == :world
+  test "even?/1 with even number" do
+    assert IsEven.even?(4) == 4
+  end
+
+  test "even?/1 with odd number" do
+    assert_raise ArgumentError, "The number 3 is not even.", fn ->
+      IsEven.even?(3)
+    end
   end
 end
